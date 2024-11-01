321ba2784e3b84f3a54d960f5ed30e5d1ba4ddd7
Lukinhas25Sx <lucasluizsx@gmail.com>
2024-11-01 00:33:02 -0300

    banco de dados na ec2 aws

--- configurations/conection.php
+++ configurations/conection.php
@@ -1,12 +1,13 @@
 <?php
 // Dados de conexão com o banco de dados
-$servername = 'localhost';
-$username = 'root';
-$password = '';
+$servername = 'ec2-44-202-161-195.compute-1.amazonaws.com'; // ou o IP da instância
+$username = 'Lucas';
+$password = 'lucas2007l';
 $database = 'banco_de_dados_colaboracao';
+$port = 3306; // a porta padrão do MySQL
 
 // Criar conexão com MySQL usando mysqli
-$conexao = new mysqli($servername, $username, $password, $database);
+$conexao = new mysqli($servername, $username, $password, $database, $port);
 
 // Verificar conexão
 if ($conexao->connect_error) {
@@ -15,7 +16,7 @@ if ($conexao->connect_error) {
 
 // Criar conexão com MySQL usando PDO (caso precise em algum lugar do projeto)
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=banco_de_dados_colaboracao;charset=utf8', $username, $password);
+    $pdo = new PDO("mysql:host=$servername;port=$port;dbname=$database;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erro de conexão: " . $e->getMessage());
